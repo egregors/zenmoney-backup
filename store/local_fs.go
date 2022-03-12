ac0e42d88c1ff405488df665054373facfff8956
egregors <root@egregors.com>
2022-03-12 23:56:27 +0100

    dev: create dir for backups

--- store/local_fs.go
+++ store/local_fs.go
@@ -1,12 +1,25 @@
 package store
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+)
+
+const downloadDir = "backups"
 
 // LocalFs is Saver to local disk
 type LocalFs struct{}
 
 // Save performs writing file to disk
 func (l LocalFs) Save(filename string, bs []byte) error {
-	err := os.WriteFile(filename, bs, 0o0600)
+	err := createDownloadDir()
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(filepath.Join(".", downloadDir, filename), bs, 0o0600)
 	return err
+}
+
+func createDownloadDir() error {
+	return os.MkdirAll(filepath.Join(".", downloadDir), os.ModePerm)
 }
